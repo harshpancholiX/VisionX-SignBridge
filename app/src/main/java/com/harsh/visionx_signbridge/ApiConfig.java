35d98460eebc5e58dc9bc88c217a3408ae43bf87
Om Rane <omrane9332@gmail.com>
2026-08-15 10:10:33 +0530

    Edited Login Activity Created Frontend + Backend

--- app/src/main/java/com/harsh/visionx_signbridge/ApiConfig.java
+++
@@ -1,7 +0,0 @@
-package com.harsh.visionx_signbridge;
-
-public class ApiConfig {
-    // Replace with your actual server URL
-    public static final String BASE_URL = "http://your-api-url.com/";
-    public static final String RESET_PASSWORD_URL = BASE_URL + "reset_password.php";
-}
