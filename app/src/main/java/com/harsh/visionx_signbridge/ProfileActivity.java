fd9f791a1d4ec8709fdb7943c46fa95f13547d6f
Om Rane <omrane9332@gmail.com>
2026-08-15 12:15:15 +0530

    Toast in profile

--- app/src/main/java/com/harsh/visionx_signbridge/ProfileActivity.java
+++ app/src/main/java/com/harsh/visionx_signbridge/ProfileActivity.java
@@ -1,6 +1,8 @@
 package com.harsh.visionx_signbridge;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ProfileActivity extends AppCompatActivity {
@@ -8,5 +10,6 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        Toast.makeText(ProfileActivity.this,"Profile open",Toast.LENGTH_SHORT).show();
     }
 }
