1b46e6055d1dd601c5a7f8fa3733375755fbdb87
HarshpancholiX <harshpancholi518@gmail.com>
2026-08-15 10:12:40 +0530

    I make HomeActivity and Add Bottom Navigation and menus and do Backend

--- app/src/main/java/com/harsh/visionx_signbridge/SettingActivity.java
+++ app/src/main/java/com/harsh/visionx_signbridge/SettingActivity.java
@@ -1,9 +1,6 @@
 package com.harsh.visionx_signbridge;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,23 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class SplashActivity extends AppCompatActivity {
+public class SettingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_setting);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        new Handler(Looper.getMainLooper()).postDelayed(() -> {
-            Intent intent = new Intent(SplashActivity.this, Forgot_Password_Activity.class);
-            startActivity(intent);
-            finish();
-        }, 3000); // 3 seconds delay
     }
 }
